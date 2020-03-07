0dc1f61e24c5e29fd2187fb9b9deec1759b29211
蒼時弦也 <elct9620@frost.tw>
2020-03-08 01:19:19 +0800

    Update package information

--- mix.exs
+++ mix.exs
@@ -4,18 +4,29 @@ defmodule Tide.MixProject do
   def project do
     [
       app: :tide,
+      description: "Communicate with Ruby via Erlport",
       version: "0.1.0",
       elixir: "~> 1.10",
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/elct9620/tide.ex"
+    ]
+  end
+
+  def package() do
+    [
+      maintainers: ["Aotokitsuruya"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/elct9620/tide.ex"
+      }
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
