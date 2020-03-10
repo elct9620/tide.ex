8d38b85845639afa8ce02a6d3300dcc87aefa2c6
蒼時弦也 <elct9620@frost.tw>
2020-03-10 18:56:11 +0800

    Add proxy method to Tide.Agent to Reaction and State

--- lib/tide/agent.ex
+++ lib/tide/agent.ex
@@ -20,7 +20,16 @@ defmodule Tide.Agent do
   def handle_call(:reaction, _from, [reaction, state]), do: {:reply, reaction, [reaction, state]}
 
   @impl true
+  def handle_call(:next, _from, [reaction, state]), do: {:reply, reaction |> Tide.Reaction.next, [reaction, state]}
+
+  @impl true
   def handle_call(:state, _from, [reaction, state]), do: {:reply, state, [reaction, state]}
+
+  @impl true
+  def handle_call({:put, key, value}, _from, [reaction, state]), do: {:reply, state |> Tide.State.put(key, value), [reaction, state]}
+
+  @impl true
+  def handle_call({:get, key}, _from, [reaction, state]), do: {:reply, state |> Tide.State.get(key), [reaction, state]}
 
   @impl true
   def handle_call({:exec, command, args}, _from, [reaction, state]), do: {:reply, Tide.Worker.exec([reaction, state |> Tide.State.to_list], command, args), [reaction, state]}
@@ -34,8 +43,29 @@ defmodule Tide.Agent do
   @doc "Get reaction"
   def reaction(pid), do: pid |> GenServer.call(:reaction)
 
+  @doc """
+  Get next reaction
+
+  See `Tide.Reaction.next/1`
+  """
+  def next(pid), do: pid |> GenServer.call(:next)
+
   @doc "Get state"
   def state(pid), do: pid |> GenServer.call(:state)
+
+  @doc """
+  Change state value
+
+  See `Tide.State.put/3`
+  """
+  def put(pid, key, value), do: pid |> GenServer.call({:put, key, value})
+
+  @doc """
+  Get state value
+
+  See `Tide.State.get/2`
+  """
+  def get(pid, key), do: pid |> GenServer.call({:get, key})
 
   @doc "Exec a event"
   def exec(pid, command, args \\ []), do: pid |> GenServer.call({:exec, command, args})
--- test/tide/agent_test.exs
+++ test/tide/agent_test.exs
@@ -14,9 +14,34 @@ defmodule Tide.AgentTest do
     end
   end
 
+  describe "Tide.Agent.put/3" do
+    test "returns :ok", %{agent: agent} do
+      agent |> Tide.Agent.put(:name, "Alice")
+      assert [] != agent |> Tide.Agent.state |> Tide.State.to_list
+    end
+  end
+
+  describe "Tide.Agent.get/2" do
+    test "returns John", %{agent: agent} do
+      agent |> Tide.Agent.state |> Tide.State.put(:name, "John")
+      assert "John" == agent |> Tide.Agent.get(:name)
+    end
+  end
+
   describe "Tide.Agent.reaction/1" do
     test "returns pid", %{agent: agent} do
       assert is_pid(agent |> Tide.Agent.reaction)
+    end
+  end
+
+  describe "Tide.Agent.next/1" do
+    test "returns next reaction", %{agent: agent} do
+      agent |> Tide.Agent.reaction |> Tide.Reaction.push(:ok)
+      assert :ok == agent |> Tide.Agent.next
+    end
+
+    test "returns nil when reaction is empty", %{agent: agent} do
+      assert nil == agent |> Tide.Agent.next
     end
   end
 
